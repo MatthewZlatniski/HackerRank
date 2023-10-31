0f604dacc3c543ee1838b393088da56548d1e552
MatthewZlatniski <147951870+MatthewZlatniski@users.noreply.github.com>
2023-10-30 21:41:10 -0400

    Create DynamicArray.py

---
+++ Data Structures/Arrays/DynamicArray.py
@@ -0,0 +1,57 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'dynamicArray' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. 2D_INTEGER_ARRAY queries
+#
+
+def dynamicArray(n, queries):
+    # Write your code here
+    arr = [None]*n
+    for i in range(n):
+        arr[i] = []
+    lastAnswer = 0
+    output = []
+    for i in range(len(queries)):
+        operations = list(map(int, queries[i]))
+        idx = 0
+        if operations[0] == 1:
+            idx = (operations[1] ^ lastAnswer) % n
+            arr[idx].append(operations[2])
+        else:
+            idx = ((operations[1] ^ lastAnswer) % n)
+            lastAnswer = arr[idx][operations[2] % len(arr[idx])]
+            output.append(lastAnswer)
+    return output
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    q = int(first_multiple_input[1])
+
+    queries = []
+
+    for _ in range(q):
+        queries.append(list(map(int, input().rstrip().split())))
+
+    result = dynamicArray(n, queries)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
